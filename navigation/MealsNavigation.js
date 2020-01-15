383504e570e2e3dfc4972b7bcf49f917eb9a270f
nkshn <vovanika23413@gmail.com>
2020-01-16 00:25:19 +0200

    Added Tabs Navigation with labels and icons

--- navigation/MealsNavigation.js
+++ navigation/MealsNavigation.js
@@ -1,11 +1,16 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 
 import Colors from '../constants/colors';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailsScreen from '../screens/MealDetailsScreen';
+import FavoriteMealsScreen from '../screens/FavoriteMealsScreen';
 
 const MealsNavigation = createStackNavigator(
   {
@@ -25,4 +30,38 @@ const MealsNavigation = createStackNavigator(
   }
 );
 
-export default createAppContainer(MealsNavigation);
+const MealsFavTabNavigation = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigation,
+      navigationOptions: {
+        tabBarLabel: 'Meals',
+        tabBarIcon: tabInfo => {
+          return (
+            <MaterialIcons
+              name="restaurant-menu"
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        }
+      }
+    },
+    Favorite: {
+      screen: FavoriteMealsScreen,
+      navigationOptions: {
+        tabBarLabel: 'Favorites',
+        tabBarIcon: tabInfo => {
+          return <AntDesign name="star" size={25} color={tabInfo.tintColor} />;
+        }
+      }
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.mainColor
+    }
+  }
+);
+
+export default createAppContainer(MealsFavTabNavigation);
