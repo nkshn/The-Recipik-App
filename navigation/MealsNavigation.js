403bc296ed436f32105424f274f1b3d59d9370e4
nkshn <vovanika23413@gmail.com>
2020-01-16 16:25:57 +0200

    Added bottom tab navigation for native Android devices

--- navigation/MealsNavigation.js
+++ navigation/MealsNavigation.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs';
+import {} from 'react-native-paper';
 
 import { MaterialIcons, AntDesign } from '@expo/vector-icons';
 
@@ -30,38 +33,44 @@ const MealsNavigation = createStackNavigator(
   }
 );
 
-const MealsFavTabNavigation = createBottomTabNavigator(
-  {
-    Meals: {
-      screen: MealsNavigation,
-      navigationOptions: {
-        tabBarLabel: 'Meals',
-        tabBarIcon: tabInfo => {
-          return (
-            <MaterialIcons
-              name="restaurant-menu"
-              size={25}
-              color={tabInfo.tintColor}
-            />
-          );
-        }
-      }
-    },
-    Favorite: {
-      screen: FavoriteMealsScreen,
-      navigationOptions: {
-        tabBarLabel: 'Favorites',
-        tabBarIcon: tabInfo => {
-          return <AntDesign name="star" size={25} color={tabInfo.tintColor} />;
-        }
+const tabScreenConfig = {
+  Meals: {
+    screen: MealsNavigation,
+    navigationOptions: {
+      tabBarLabel: 'Meals',
+      tabBarIcon: tabInfo => {
+        return (
+          <MaterialIcons
+            name="restaurant-menu"
+            size={25}
+            color={tabInfo.tintColor}
+          />
+        );
       }
     }
   },
-  {
-    tabBarOptions: {
-      activeTintColor: Colors.mainColor
+  Favorite: {
+    screen: FavoriteMealsScreen,
+    navigationOptions: {
+      tabBarLabel: 'Favorites',
+      tabBarIcon: tabInfo => {
+        return <AntDesign name="star" size={25} color={tabInfo.tintColor} />;
+      }
     }
   }
-);
+};
+
+const MealsFavTabNavigation =
+  Platform.OS === 'android'
+    ? createMaterialBottomTabNavigator(tabScreenConfig, {
+        activeColor: Colors.mainColor,
+        barStyle: {
+          backgroundColor: Colors.lighterGrey
+        },
+        shifting: true
+      })
+    : createBottomTabNavigator(tabScreenConfig, {
+        tabBarOptions: { activeTintColor: Colors.mainColor }
+      });
 
 export default createAppContainer(MealsFavTabNavigation);
