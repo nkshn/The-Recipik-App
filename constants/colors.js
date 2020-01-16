0988c1aef4310542d17f0da3b95ec9af55b24445
nkshn <vovanika23413@gmail.com>
2020-01-16 16:22:19 +0200

    Changes in colors

--- constants/colors.js
+++ constants/colors.js
@@ -1,7 +1,8 @@
 export default {
-  mainColor: '#d95b52', // red
+  mainColor: '#d95b52',
   orange: '#d97b52',
   blue: '#3698c9',
   grey: '#777',
-  white: '#fff'
+  white: '#fff',
+  lighterGrey: '#d4d4d4'
 };
